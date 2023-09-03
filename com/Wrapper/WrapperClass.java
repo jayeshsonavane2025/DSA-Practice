281ec88b03b512822f9a3cd4f2da756befb111c8
jayeshsonavane2025 <jayeshsonavane2025@gmail.com>
2023-09-03 08:14:21 +0530

    Wrapper class

---
+++ com/Wrapper/WrapperClass.java
@@ -0,0 +1,9 @@
+package com.Wrapper;
+
+public class WrapperClass {
+    public static void main(String[] args) {
+        Integer a=Integer.valueOf(3);
+        System.out.println(a);
+        System.out.println(String.valueOf("Radhe"));
+    }
+}
