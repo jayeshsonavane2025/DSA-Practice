d723ef21110652e0a0327cc2fa6ea5c414394b39
jayeshsonavane2025 <jayeshsonavane2025@gmail.com>
2023-09-05 12:59:22 +0530

    Collection Interface

---
+++ collectionAndLists/CollectionList.java
@@ -0,0 +1,39 @@
+package collectionAndLists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionList {
+    public static void main(String[] args) {
+        List<Integer> list=new ArrayList<>();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+        System.out.println("Size of list "+list.size());
+
+        System.out.println(list);
+        list.remove(2);
+
+        list.remove(Integer.valueOf(20));
+        list.remove(Integer.valueOf(20));
+
+        System.out.println(list.contains(20));
+
+
+        List<Integer> list2=new ArrayList<>();
+
+        list2.add(100);
+        list2.addAll(list);
+
+        System.out.println(list2);
+
+
+    }
+
+
+}
