43ef131b983b91bb17417fed98d4428a1f55cb93
jayeshsonavane2025 <jayeshsonavane2025@gmail.com>
2023-09-07 13:21:25 +0530

    Queue interface

---
+++ collectionAndLists/QueueImplement.java
@@ -0,0 +1,24 @@
+package collectionAndLists;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueImplement {
+    public static void main(String[] args) {
+        Queue<Integer> q=new LinkedList<>();
+
+        q.offer(1);
+        q.offer(2);
+        System.out.println(q);
+
+        System.out.println(q.poll());
+
+        System.out.println(q);
+
+        System.out.println(q.peek());
+
+       
+
+        System.out.println(q);
+    }
+}
