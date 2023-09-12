f0d09862e241a2bafa2e807d7a39462c6a7d6514
jayeshsonavane2025 <jayeshsonavane2025@gmail.com>
2023-09-12 22:58:33 +0530

    1200. Minimum Absolute Difference

---
+++ arrays2/MinimumAbsoluteDifference.java
@@ -0,0 +1,27 @@
+package arrays2;
+
+import java.util.*;
+
+public class MinimumAbsoluteDifference {
+    public List<List<Integer>> minimumAbsDifference(int[] a) {
+        List<List<Integer>> list=new ArrayList<>();
+        Arrays.sort(a);
+        int minDiff=Integer.MAX_VALUE;
+        for(int i=1;i<a.length;i++){
+            //minDiff=Math.min(minDiff,Math.abs(a[i]-a[i-1]));
+            if(minDiff>Math.abs(a[i]-a[i-1])){
+                minDiff=Math.abs(a[i]-a[i-1]);
+            }
+        }
+        for(int i=1;i<a.length;i++){
+            if(Math.abs(a[i]-a[i-1])==minDiff){
+                List<Integer> temp=new ArrayList<>();
+                temp.add(a[i-1]);
+                temp.add(a[i]);
+                list.add(temp);
+
+            }
+        }
+        return list;
+    }
+}
