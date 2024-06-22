67ae607d4667661fccf90435a08609c20bf3adc6
Jayesh Sonavane <jayeshsonavane2025@gmail.com>
2024-06-22 20:51:23 +0530

    Update CollectionList.java

--- collectionAndLists/CollectionList.java
+++ collectionAndLists/CollectionList.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+
 public class CollectionList {
     public static void main(String[] args) {
         List<Integer> list=new ArrayList<>();
